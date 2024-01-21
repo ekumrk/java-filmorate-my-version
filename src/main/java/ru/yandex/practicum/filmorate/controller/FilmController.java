4554cac7edb4eeaaacb1b5df76ab7ecda8ab4ebf
EugeneK <ekum.rk@yandex.ru>
2024-01-21 22:47:00 +0300

    checkstyle6

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.storage.exceptions.DataNotFoundException;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
