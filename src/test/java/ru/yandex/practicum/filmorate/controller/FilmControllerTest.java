ebe72541bf000447cd8658fdfbf2a66591b7a665
EugeneK <ekum.rk@yandex.ru>
2024-01-27 01:57:33 +0300

    tests2

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,84 +1,57 @@
 package ru.yandex.practicum.filmorate.controller;
 
+
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class FilmControllerTest {
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mvc;
 
     @Test
-    void shouldAddFilmAndUpdateFilm() throws Exception {
-        //Добавление фильма
-        mockMvc.perform(post("/films")
+    void createFilmWithBadDate() throws Exception {
+        mvc.perform(post("/films")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\": \"Film name\", \"description\": \"Film description\"," +
-                                " \"releaseDate\": \"1967-03-25\", \"duration\": 100}"))
-                .andExpect(status().isOk());
-
-        mockMvc.perform(get("/films"))
-                .andExpect(content().string("[{\"name\":\"Film name\",\"description\":\"Film description\"," +
-                        "\"releaseDate\":\"1967-03-25\",\"duration\":100,\"id\":1,\"likes\":[]}]"));
-
-        //Обновление фильма
-        mockMvc.perform(put("/films")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"id\": \"1\",\"name\": \"FilmNameUpdate\", \"description\": \"Film description\"," +
-                                " \"releaseDate\": \"1967-03-25\", \"duration\": 100}"))
-                .andExpect(status().isOk());
-
-        mockMvc.perform(get("/films"))
-                .andExpect(content().string("[{\"name\":\"FilmNameUpdate\",\"description\":\"Film description\"," +
-                        "\"releaseDate\":\"1967-03-25\",\"duration\":100,\"id\":1,\"likes\":[]}]"));
+                        .content("{\"name\": \"Film name\", \"description\": \"Film descr\", \"releaseDate\": \"1890-03-25\", \"duration\": 100}"))
+                .andExpect(status().is(400));
     }
 
     @Test
-    void shouldAddFilmAndUpdateFilmUnsuccessfully() throws Exception {
-        //фильм с пустым названием
-        mockMvc.perform(post("/films/")
+    void createFilmWithTooLongDescription() throws Exception {
+        mvc.perform(post("/films")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\": \"\", \"description\": \"Film description\"," +
-                                " \"releaseDate\": \"1967-03-25\", \"duration\": 100}"))
-                .andExpect(status().is5xxServerError());
-
-        //фильм со слишком длинным описанием
-        mockMvc.perform(post("/films/")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\": \"FilmName\", \"description\": \"Film descriptionFilm descriptionFilm " +
-                                "descriptionFilm descriptionFilm descriptionFilm descriptionFilm descriptionFilm " +
-                                "descriptionFilm descriptionFilm descriptionFilm descriptionFilm descriptionFilm " +
-                                "descriptionFilm descriptionFilm descriptionFilm descriptionFilm descriptionFilm " +
-                                "descriptionFilm descriptionFilm description Film descriptionFilm descriptionFilm " +
-                                "descriptionFilm descriptionFilm descriptionFilm descriptionFilm description\"," +
-                                " \"releaseDate\": \"1967-03-25\", \"duration\": 100}"))
-                .andExpect(status().is5xxServerError());
-
-        //обновление несуществующего фильма
-        mockMvc.perform(put("/films/")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"id\": \"2\",\"name\": \"FilmNameUpdate\", \"description\": \"Film description\"," +
-                                " \"releaseDate\": \"1967-03-25\", \"duration\": 100}"))
-                .andExpect(status().is4xxClientError());
-
-        //фильм с неправильной датой выпуска
-        mockMvc.perform(put("/films/")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\": \"FilmName\", \"description\": \"Film description\"," +
-                                " \"releaseDate\": \"1890-03-25\", \"duration\": 100}"))
-                .andExpect(status().is5xxServerError());
+                        .content("{\"name\": \"Film name\", \"description\": \"Пятеро друзей ( комик-группа «Шарло»), приезжают в город Бризуль. Здесь они хотят разыскать господина Огюста Куглова, который задолжал им деньги, а именно 20 миллионов. о Куглов, который за время «своего отсутствия», стал кандидатом Коломбани.\", \"releaseDate\": \"1990-03-25\", \"duration\": 100}"))
+                .andExpect(status().is(400));
     }
 
+    @Test
+    void createFilmWithEmptyName() throws Exception {
+        mvc.perform(post("/films")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"\", \"description\": \"Descr\", \"releaseDate\": \"1990-03-25\", \"duration\": 100}"))
+                .andExpect(status().is(400));
+    }
+
+    @Test
+    void createFilmWithNegativeDuration() throws Exception {
+        mvc.perform(post("/films")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"Film name\", \"description\": \"Film descr\", \"releaseDate\": \"1967-03-25\", \"duration\": -100}"))
+                .andExpect(status().is(400));
+    }
 }
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -1,75 +1,64 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class UserControllerTest {
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class UserControllerTest {
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mvc;
 
     @Test
-    void shouldAddAndUpdateUserSuccessfully() throws Exception {
-        //пользователь с пустым именем
-        mockMvc.perform(post("/users")
+    void createUserWithInvalidLogin() throws Exception {
+        mvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"login\": \"dolore\", \"name\": \"\", \"email\": \"mail@mail.ru\"," +
-                                " \"birthday\": \"1946-08-20\"}"))
-                .andExpect(status().isOk());
-
-        mockMvc.perform(get("/users"))
-                .andExpect(content().string("[{\"email\":\"mail@mail.ru\",\"login\":\"dolore\"," +
-                        "\"name\":\"dolore\",\"birthday\":\"1946-08-20\",\"id\":1,\"friends\":[]}]"));
-
-        //Обновление пользователя
-        mockMvc.perform(put("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"email\":\"mail@mail.ru\",\"login\":\"dolore\",\"name\":\"doloreUpdate\"," +
-                                "\"birthday\":\"1946-08-20\",\"id\":1}"))
-                .andExpect(status().isOk());
-
-        mockMvc.perform(get("/users"))
-                .andExpect(content().string("[{\"email\":\"mail@mail.ru\",\"login\":\"dolore\"," +
-                        "\"name\":\"doloreUpdate\",\"birthday\":\"1946-08-20\",\"id\":1,\"friends\":[]}]"));
+                        .content("{\n" +
+                                "  \"login\": \"dolore ullamco\",\n" +
+                                "  \"email\": \"yandex@mail.ru\",\n" +
+                                "  \"birthday\": \"2446-08-20\"\n" +
+                                "}"))
+                .andExpect(status().is(400));
     }
+
 
     @Test
-    void shouldAddUserAndUpdateUserUnsuccessfully() throws Exception {
-        //пользователь с неправильным email
-        mockMvc.perform(post("/users/")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"login\": \"dolore\",\"email\": \"mailmail.ru\"," +
-                        " \"birthday\": \"1946-08-20\"}")).andExpect(status().is5xxServerError());
-
-
-        //обновление несуществующего пользователя
-        mockMvc.perform(put("/users/")
+    void createUserWithInvalidEmail() throws Exception {
+        mvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"login\": \"doloreUpdate\",\"name\": \"est adipisicing\",\"id\": 3," +
-                                "\"email\": \"mail@yandex.ru\",\"birthday\": \"1976-09-20\"}"))
-                .andExpect(status().is4xxClientError())
-                .andExpect(result -> assertEquals("Ошибка! Такого пользователя не существует!",
-                        result.getResolvedException().getMessage()));
-
-        //пользователь с неправильным email
-        mockMvc.perform(MockMvcRequestBuilders.post("/users/")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"login\": \"dolore\",\"email\": \"mailmail.ru\"," +
-                        " \"birthday\": \"1946-08-20\"}")).andExpect(status().is5xxServerError());
+                        .content("{\n" +
+                                "  \"login\": \"dolore ullamco\",\n" +
+                                "  \"name\": \"\",\n" +
+                                "  \"email\": \"mail.ru\",\n" +
+                                "  \"birthday\": \"1980-08-20\"\n" +
+                                "}"))
+                .andExpect(status().is(400));
     }
 
+
+    @Test
+    void createUserWithInvalidBirthday() throws Exception {
+        mvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "  \"login\": \"dolore ullamco\",\n" +
+                                "  \"name\": \"\",\n" +
+                                "  \"email\": \"mail.ru\",\n" +
+                                "  \"birthday\": \"1980-08-20\"\n" +
+                                "}"))
+                .andExpect(status().is(400));
+    }
 }
