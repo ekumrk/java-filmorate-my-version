7faa57acc1caa6c5a39ce8d7896dd8b20055fd25
EugeneK <ekum.rk@yandex.ru>
2024-01-27 12:26:44 +0300

    fix_after_review

--- src/main/java/ru/yandex/practicum/filmorate/model/supportive/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/supportive/Genre.java
@@ -2,9 +2,7 @@ package ru.yandex.practicum.filmorate.model.supportive;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Service;
 
-@Service
 @Getter
 @Setter
 public class Genre extends ModelEntity {
