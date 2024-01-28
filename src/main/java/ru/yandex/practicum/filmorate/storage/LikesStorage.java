8acf864c6b7eec8f0588c7ae958e4f944c2c95c0
EugeneK <ekum.rk@yandex.ru>
2024-01-28 13:36:34 +0300

    fix_after_review

--- src/main/java/ru/yandex/practicum/filmorate/storage/LikesStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/LikesStorage.java
@@ -5,6 +5,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface LikesStorage {
+
     void addLike(int filmId, int userId);
 
     void removeLike(int filmId, int userId);
